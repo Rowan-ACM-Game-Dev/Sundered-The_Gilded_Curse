dd1d7fb7f09ab1791b412abb8db9d275b762d781
Joshua Odom <94143587+JoshuaO22@users.noreply.github.com>
2025-04-14 17:22:30 -0400

    Add timed door
    Scene changes:
    Changed color of room 4 lever (white to green)
    Added 2d box collider to lever with trigger enabled
    Disabled trigger on room 4 door
    Added door game object reference to lever script

--- Assets/Room4TimedButton.cs
+++ Assets/Room4TimedButton.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Room4TimedButton : MonoBehaviour
 {
+    public GameObject door;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +16,21 @@ public class Room4TimedButton : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && door.activeSelf)
+        {
+            StartCoroutine(ActivateDoorTimed(5));
+        }
+
+    }
+
+    IEnumerator ActivateDoorTimed(int waitSeconds)
+    {
+        door.SetActive(false);
+        yield return new WaitForSeconds(waitSeconds);
+        door.SetActive(true);
     }
 }
